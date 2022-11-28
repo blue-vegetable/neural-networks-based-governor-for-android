5566b16e78ec98829668217e3d0392e348a0ef64
Blues <blues243134@gmail.com>
2022-11-28 21:25:34 +0800

    开机自启动

--- app/src/main/java/com/example/networktrans/MyService.java
+++ app/src/main/java/com/example/networktrans/MyService.java
@@ -1,32 +1,30 @@
 package com.example.networktrans;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.IBinder;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
 
 import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 
-
-public class MainActivity extends AppCompatActivity {
-    private Button btn_1;
-    private Button btn_2;
-    private Button btn_equ;
-    private TextView output;
+public class MyService extends Service {
     private TFLiteClassificationUtil tfLiteClassificationUtil;
-    private ArrayList<Float> buffer = new ArrayList<>();
     String view;
-
     TensorBuffer x =  TensorBuffer.createDynamic(DataType.FLOAT32);
     TensorBuffer y =  TensorBuffer.createDynamic(DataType.FLOAT32);
+
 
     public class SystemInfoThread extends Thread{
         private String view;
@@ -48,84 +46,84 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     Thread.sleep(1000);
                     calculate();
+//                    Toast.makeText(MyService.this, "running", Toast.LENGTH_SHORT).show();
 
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         }
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        init();
-        Log.d("TAG","activity here");
-//        setEventListener();
-//        try {
-//            long t1 = System.currentTimeMillis();
-//            loadModel();
-//            long t2 = System.currentTimeMillis();
-//            Log.d("TAG","load model takes " + (t2 - t1));
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        String command = "echo \"userspace\" >  /sys/devices/system/cpu/cpufreq/policy0/scaling_governor"; // little cpu governor
-//        CommandExecution.execCommand(command,true);
-//        command = "echo \"userspace\" >  /sys/devices/system/cpu/cpufreq/policy4/scaling_governor";  // big cpu governor
-//        CommandExecution.execCommand(command,true);
-//        SystemInfoThread sit = new SystemInfoThread(view);
-//        sit.start();
     }
 
     private void init() {
-        btn_1 =findViewById(R.id.button1);
-        btn_2 = findViewById(R.id.button2);
-        btn_equ = findViewById(R.id.button3);
-        output = findViewById(R.id.textView);
         view = "com.ss.android.ugc.aweme/com.ss.android.ugc.aweme.splash.SplashActivity#0";
     }
 
-    private void setEventListener(){
-        btn_1.setOnClickListener(ClickInHere);
-        btn_2.setOnClickListener(ClickInHere);
-        btn_equ.setOnClickListener(ClickInHere);
+    public MyService() {
     }
 
-    private View.OnClickListener ClickInHere = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            switch(v.getId()) {
-                case R.id.button1: buffer.add(1.0F); output.setText("1");
-                    try {
-                        Runtime.getRuntime().exec("su");
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                case R.id.button2: buffer.add(2.0F);  output.setText("2"); break;
-                case R.id.button3: try {
-                    calculate();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                default: break;
-            }
+    private String createNotificationChannel(String channelId, String channelName){
+        NotificationChannel chan = new NotificationChannel(channelId,
+                channelName, NotificationManager.IMPORTANCE_NONE);
+        chan.setLightColor(Color.BLUE);
+        chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        NotificationManager service = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        service.createNotificationChannel(chan);
+        return channelId;
+    }
+
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+        init();
+        Toast.makeText(this, "My Service Started", Toast.LENGTH_LONG).show();
+        Log.d("TAG", "onStart");
+        String channelId = null;
+        // 8.0 以上需要特殊处理
+        channelId = createNotificationChannel("kim.hsl", "ForegroundService");
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId);
+        Notification notification = builder.setOngoing(true)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setPriority(1)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .build();
+        startForeground(1, notification);
+
+        try {
+            long t1 = System.currentTimeMillis();
+            loadModel();
+            long t2 = System.currentTimeMillis();
+            Log.d("TAG","load model takes " + (t2 - t1));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    };
+        String command = "echo \"userspace\" >  /sys/devices/system/cpu/cpufreq/policy0/scaling_governor"; // little cpu governor
+        CommandExecution.execCommand(command,true);
+        command = "echo \"userspace\" >  /sys/devices/system/cpu/cpufreq/policy4/scaling_governor";  // big cpu governor
+        CommandExecution.execCommand(command,true);
+        MyService.SystemInfoThread sit = new MyService.SystemInfoThread(view);
+        sit.start();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return null;
+    }
 
     private void loadModel() {
         String classificationModelPath = getCacheDir().getAbsolutePath() + File.separator + "model.tflite";
-        Utils.copyFileFromAsset(MainActivity.this, "model.tflite", classificationModelPath);
+        Utils.copyFileFromAsset(MyService.this, "model.tflite", classificationModelPath);
         // load the model
         try {
             tfLiteClassificationUtil = new TFLiteClassificationUtil(classificationModelPath);
-            Toast.makeText(MainActivity.this, "load model succeeded", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MyService.this, "load model succeeded", Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
-            Toast.makeText(MainActivity.this, "load model failed", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MyService.this, "load model failed", Toast.LENGTH_SHORT).show();
             e.printStackTrace();
-            finish();
         }
     }
 
